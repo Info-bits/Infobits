d00d70707c03b88ab615e12bf64a86377627fd15
Info-bits <mhmdyzn2000@gmail.com>
2024-11-28 09:43:39 +0530

    Add files via upload

---
+++ Files/script.js
@@ -0,0 +1,13 @@
+function filterArticles(category) {
+    const articles = document.querySelectorAll('.post-card');
+
+    articles.forEach(article => {
+        if (category === 'all') {
+            article.style.display = 'block';
+        } else if (article.classList.contains(category)) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
+    });
+}
